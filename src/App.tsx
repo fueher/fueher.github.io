2ed24c8e6478594d1190780329e669143e1c7929
Fue Her <35857172+fueher@users.noreply.github.com>
2024-09-22 03:38:16 +0000

    Updated landing page (about me)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import profile from './assets/profile.jpg';
+import './App.css';
+import { Avatar, Card, Button } from 'antd';
+import { GithubOutlined, LinkedinOutlined } from '@ant-design/icons';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="/">
+          <Avatar className={"logo react"} size={'large'} src={profile} />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Fue Her</h1>
+      <Card title={"About me"}>
+        <p>Self-motivated person who is passionate on learning new and emerging
+          technologies with a extensive knowledge of computer science concepts and have high expectations of being
+          utilized to make positive contribution to the industry.</p>
+        <div style={{ paddingTop: 8 }}>
+          <Button type={"link"} href={'https://www.github.com/fueher/fueher.github.io'} target={'_blank'}>
+            <GithubOutlined style={{ color: 'black', fontSize: 50 }} />
+          </Button>
+          <Button type={"link"} href={'https://www.linkedin.com/in/fue-her-8ba2a0114/'} target={'_blank'}>
+            <LinkedinOutlined style={{ fontSize: 50, color: '#0a66c2' }} />
+          </Button>
+        </div>
+      </Card>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
